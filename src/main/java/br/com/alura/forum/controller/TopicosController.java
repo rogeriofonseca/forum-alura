2231ef2b34eea9549a4ffa04a7a12d516ac531f1
Rogério Fonseca Gonçalves <rogerio.fonseca@gmail.com>
2021-12-03 21:50:49 -0300

    Adding pagination on endpoint [topicos];

--- src/main/java/br/com/alura/forum/controller/TopicosController.java
+++ src/main/java/br/com/alura/forum/controller/TopicosController.java
@@ -8,6 +8,9 @@ import javax.transaction.Transactional;
 import javax.validation.Valid;
 
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.data.domain.Page;
+import org.springframework.data.domain.PageRequest;
+import org.springframework.data.domain.Pageable;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.DeleteMapping;
 import org.springframework.web.bind.annotation.GetMapping;
@@ -16,6 +19,7 @@ import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.PutMapping;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.RestController;
 import org.springframework.web.util.UriComponentsBuilder;
 
@@ -38,12 +42,15 @@ public class TopicosController {
     private CursoRepository cursoRepository;
 
     @GetMapping
-    public List<TopicoDto> lista(String param) {
-        List<Topico> topicos = null;
-        if(param == null){
-             topicos = topicoRepository.findAll();
+    public Page<TopicoDto> lista(@RequestParam(required = false) String nomeCurso, 
+            @RequestParam int pagina, @RequestParam int itensPorPagina) {
+ 
+        Pageable paginacao = PageRequest.of(pagina, itensPorPagina);
+        Page<Topico> topicos = null;
+        if(nomeCurso == null){
+            topicos = topicoRepository.findAll(paginacao);
         } else {
-            topicos = topicoRepository.findByCursoNome(param);
+            topicos = topicoRepository.findByCursoNome(nomeCurso, paginacao);
         }
 
         return TopicoDto.converter(topicos);
